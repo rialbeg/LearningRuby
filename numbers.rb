4e91ec7ab76c355c4a6c9874c514cbed080097f2
rialbeg <gsamascarenhas@gmail.com>
2023-05-05 21:49:26 -0300

    finishing learning numbers

---
+++ numbers.rb
@@ -0,0 +1,12 @@
+# integer
+42
+
+#float
+42.2
+
+# thousands separator
+1_000_000 # 1000000
+
+# numbers are objects and operations symbols as well
+
+p 5.public_methods # :+ is a method of Integer class
