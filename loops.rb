a07daa894fabe7ce15000e7a2833a46de8faf67c
rialbeg <gsamascarenhas@gmail.com>
2023-05-08 21:25:39 -0300

    finishing learning loops

---
+++ loops.rb
@@ -0,0 +1,38 @@
+# while syntax
+
+# while conditional [do]
+#     code
+# end
+
+# do while syntax
+
+# begin 
+#     code 
+# end while conditional
+
+# until syntax
+
+# until is a while but the conditional is false
+
+# until conditional [do]
+#     code
+# end
+
+# do until syntax
+# begin
+#     code
+# end until conditional
+
+# for syntax
+
+# for variable [, variable ...] in expression [do]
+#     code
+# end
+
+# using the each method
+
+# (expression).each do |variable[, variable...]| code end
+
+# break breaks out of immediately up loop
+# next stops execution and continues from next operation in loop
+# redo stops execution and restart the loop
